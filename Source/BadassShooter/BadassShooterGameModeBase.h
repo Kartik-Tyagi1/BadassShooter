b2c13092c62b9c264c1e63d841b8e5f1938ac20a
Kartik-Tyagi1 <kartiktyagi278@gmail.com>
2021-12-22 13:25:25 -0500

    Initial Commit

---
+++ Source/BadassShooter/BadassShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BadassShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BADASSSHOOTER_API ABadassShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
